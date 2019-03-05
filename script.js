f697a5a4ce4b08283cef605445b11b83e0696995
Zhuowei233 <36427788+Zhuowei233@users.noreply.github.com>
2019-03-04 16:31:11 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,38 @@
+$(".button2").click(function(){
+    $(".picture").attr("src","https://d3i6fh83elv35t.cloudfront.net/newshour/app/uploads/2017/04/IMG_5316-1024x768.jpg");
+$(".pp").text("Hello Im Mr.Stevens a science teacher for Physics ");
+$(".star").attr("src","http://clipart-library.com/images/gceoXx7gi.png");
+
+});
+
+$(".button3").click(function(){
+    $(".picture").attr("src","https://pbs.twimg.com/profile_images/722667318636101633/g3yfdSug_400x400.jpg");
+    $(".pp").text("Hello my name is Arnold and Im a PE teacher ");
+    $(".star").attr("src","http://www.lhps.co.uk/wp-content/uploads/2016/02/3-star-service-contract.jpg");
+});
+
+$(".button4").click(function(){
+    $(".picture").attr("src","https://www.barnesandnoble.com/blog/barnesy/wp-content/uploads/2013/09/mean_teacher_ruler.jpg");
+    $(".pp").text(" Hello. My name is Anne and I am a Geometry teacher");
+    $(".star").attr("src","https://wheatbeerexpert.files.wordpress.com/2014/12/1-star.png");
+});
+
+$(".button5").click(function(){
+    $(".picture").attr("src","http://media2.govtech.com/images/940*571/Computer_Science_Teacher_CDE_2015-12-14.jpg");
+    $(".pp").text("Hello my name is Mr.Andreson and I am a computer science teacher");
+    $(".star").attr("src","https://www.unitedwayofacadiana.org/sites/unitedwayofacadiana.org/files/four%20star.png");
+});
+
+$(".button6").click(function(){
+    $(".picture").attr("src","https://cdn-images-1.medium.com/max/1600/0*UabFjZCc8yJ0JK3x.jpg");
+    $(".pp").text("Hello My name is Mr.Elon and I am a economy teacher");
+    $(".star").attr("src","https://www.judsonsmartliving.org/wp-content/uploads/5-stars.png");
+});
+
+$(".button1").click(function(){
+    $(".picture").attr("src","http://www.middlesex.k12.nj.us/msd/_zumu_user_image_cache/teacher.jpg");
+});
+
+$(".survey").click(function(){
+  
+});
